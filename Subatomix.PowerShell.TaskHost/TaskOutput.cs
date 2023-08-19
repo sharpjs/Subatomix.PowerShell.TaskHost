04b9fc303663bfead0e7135f671c7aa1e727b355
Jeff Sharp <public@sharpjs.net>
2023-08-19 07:19:17 -0700

    Documentation.

--- Subatomix.PowerShell.TaskHost/TaskOutput.cs
+++ Subatomix.PowerShell.TaskHost/TaskOutput.cs
@@ -4,11 +4,23 @@
 namespace Subatomix.PowerShell.TaskHost;
 
 /// <summary>
-///   A wrapper that carries a <see cref="TaskInfo"/> reference along with an
-///   output object.
+///   A pair consisting of a task associated with an output object.
 /// </summary>
 internal class TaskOutput
 {
+    /// <summary>
+    ///   Initializes a new <see cref="TaskOutput"/> instance with the
+    ///   specified task and object.
+    /// </summary>
+    /// <param name="task">
+    ///   The task associated with <paramref name="obj"/>.
+    /// </param>
+    /// <param name="obj">
+    ///   The output object.
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    ///   <paramref name="task"/> is <see langword="null"/>.
+    /// </exception>
     public TaskOutput(TaskInfo task, object? obj)
     {
         if (task is null)
@@ -18,7 +30,13 @@ internal class TaskOutput
         Object = obj;
     }
 
+    /// <summary>
+    ///   Gets the task associated with <see cref="Object"/>.
+    /// </summary>
     public TaskInfo Task { get; }
 
+    /// <summary>
+    ///   Gets the output object.
+    /// </summary>
     public object? Object { get; }
 }
