6d00a0aa36f6e3c02c921164d139fd1a6a3a4a72
Jeff Sharp <public@sharpjs.net>
2023-08-27 17:15:59 -0700

    Add ability to check tests for task hygiene.

--- Subatomix.PowerShell.TaskHost.Tests/TestSuite.cs
+++ Subatomix.PowerShell.TaskHost.Tests/TestSuite.cs
@@ -3,3 +3,17 @@
 
 [assembly: Parallelizable(ParallelScope.All)]
 [assembly: FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+
+#if ENFORCE_GOOD_HYGIENE
+namespace Subatomix.PowerShell.TaskHost;
+
+[SetUpFixture]
+public static class TestSuite
+{
+    [OneTimeTearDown]
+    public static void TearDown()
+    {
+        TaskInfo.All.Should().BeEmpty();
+    }
+}
+#endif
