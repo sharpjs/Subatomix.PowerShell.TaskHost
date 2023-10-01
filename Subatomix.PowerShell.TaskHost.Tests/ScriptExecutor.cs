12d49024a1e4bafcd23903f66d9f40efce7a4ab4
Jeff Sharp <public@sharpjs.net>
2023-09-30 18:24:59 -0700

    Remove unneeded parts.

--- Subatomix.PowerShell.TaskHost.Tests/ScriptExecutor.cs
+++ Subatomix.PowerShell.TaskHost.Tests/ScriptExecutor.cs
@@ -1,7 +1,6 @@
 // Copyright 2023 Subatomix Research Inc.
 // SPDX-License-Identifier: ISC
 
-using System.Management.Automation.Remoting.Internal;
 using System.Runtime.InteropServices;
 using Microsoft.PowerShell;
 
@@ -43,8 +42,6 @@ internal static class ScriptExecutor
 
         using var shell = Sma.PowerShell.Create(InitialState);
 
-        Redirect(shell.Streams, output);
-
         shell
             .AddCommand("Set-Location").AddParameter("LiteralPath", TestPath)
             .AddScript(script);
@@ -60,27 +57,4 @@ internal static class ScriptExecutor
 
         return (output, exception);
     }
-
-    private static void Redirect(PSDataStreams streams, List<PSObject?> output)
-    {
-        streams.Warning.DataAdding += (_, data) => StoreWarning (data, output);
-        streams.Error  .DataAdding += (_, data) => StoreError   (data, output);
-    }
-
-    private static void StoreWarning(DataAddingEventArgs data, List<PSObject?> output)
-    {
-        var written = (WarningRecord) data.ItemAdded;
-        var message = new PSWarning(written.Message);
-        output.Add(new PSObject(message));
-    }
-
-    private static void StoreError(DataAddingEventArgs data, List<PSObject?> output)
-    {
-        var written = (ErrorRecord) data.ItemAdded;
-        var message = new PSError(written.Exception.Message);
-        output.Add(new PSObject(message));
-    }
 }
-
-internal record PSWarning (string Message);
-internal record PSError   (string Message);
