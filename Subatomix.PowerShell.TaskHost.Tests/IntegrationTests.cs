25b67a687c9527e5f9c4fdedd9b0fa5eeb6f8839
Jeff Sharp <public@sharpjs.net>
2023-09-30 18:33:12 -0700

    Add more integration test coverage.

--- Subatomix.PowerShell.TaskHost.Tests/IntegrationTests.cs
+++ Subatomix.PowerShell.TaskHost.Tests/IntegrationTests.cs
@@ -4,6 +4,7 @@
 namespace Subatomix.PowerShell.TaskHost;
 
 [TestFixture]
+[NonParallelizable]
 public class IntegrationTests : TestHarnessBase
 {
     private readonly Mock<PSHost>                 _host;
@@ -48,7 +49,7 @@ public class IntegrationTests : TestHarnessBase
             $TestVarA = 42
             $TestVarB = "eh"
 
-            Use-TaskHost {
+            Use-TaskHost -WithElapsed {
                 Invoke-Task TaskX -UseHost $Host {
                     Use-TestModuleA
                     Use-TestModuleB
@@ -66,6 +67,71 @@ public class IntegrationTests : TestHarnessBase
             new PSObject(42 as object),
             new PSObject("eh")
         });
+
+        exception.Should().BeNull();
+    }
+
+    [Test]
+    public void NestedUseTaskHost()
+    {
+        var (output, exception) = ScriptExecutor.Execute(
+            _host.Object,
+            """
+            Use-TaskHost -WithElapsed {
+                Use-TaskHost -WithElapsed {
+                    Invoke-Task Test { "foo" }
+                }
+            }
+            """
+        );
+
+        output.Should().BeEquivalentTo(new[]
+        {
+            new PSObject("foo")
+        });
+
+        exception.Should().BeNull();
+    }
+
+    [Test]
+    public void Bypassed()
+    {
+        var (output, exception) = ScriptExecutor.Execute(
+            _host.Object,
+            """
+            $TaskHostBypass = $true # anything except $null or $false
+            Use-TaskHost {
+                Invoke-Task Test { "foo" }
+            }
+            """
+        );
+
+        output.Should().BeEquivalentTo(new[]
+        {
+            new PSObject("foo")
+        });
+
+        exception.Should().BeNull();
+    }
+
+    [Test]
+    public void NullUI()
+    {
+        _ui.Setup(u => u.WriteWarningLine(
+            "A Use-TaskHost command will have no effect because $Host.UI is null."
+        )).Verifiable();
+
+        var (output, exception) = ScriptExecutor.Execute(
+            _host.Object,
+            """
+            1 | ForEach-Object -Parallel {
+                # $Host.UI is $null here
+                Use-TaskHost { }
+            }
+            """
+        );
+
+        output   .Should().BeEmpty();
         exception.Should().BeNull();
     }
 }
